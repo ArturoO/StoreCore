4aadde5d2684edda243c81a93c3c9d2f13455a65
Artur Hoffman <thearturoo87@gmail.com>
2018-07-01 20:00:28 +0200

    Move console related code from Program class to separate MainUI class.

---
+++ StoreCore/MainUI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreCore
+{
+    class MainUI: IConsoleUI
+    {
+        protected Dictionary<string, IConsoleUI> consoleUIs;
+        protected Dictionary<string, Action> commandsMap;
+
+        public MainUI()
+        {
+            consoleUIs = new Dictionary<string, IConsoleUI>();
+            consoleUIs.Add("MainUI", this);
+            consoleUIs.Add("UserUI", new UserUI());
+            consoleUIs.Add("ProductUI", new ProductUI());
+
+            commandsMap = new Dictionary<string, Action>();
+            foreach (var consoleUI in consoleUIs)
+            {
+                consoleUI.Value.registerCommands(commandsMap);
+            }
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Welcome to our store!\r\nHow can we help you?");
+            Console.WriteLine("To display a list of all available commands type: help");
+
+            while (true)
+            {
+                String input = Console.ReadLine();
+
+                if (input == "exit")
+                    break;
+                else if (commandsMap.ContainsKey(input))
+                {
+                    commandsMap[input]();
+                }
+                else
+                    Console.WriteLine("Incorrect command. Type 'help' for a list of commands. Type 'exit' to exit the program.");
+            }
+        }
+
+
+        public void registerCommands(Dictionary<string, Action> commandsMap)
+        {
+            commandsMap.Add("help", help);
+        }
+
+
+        public void help()
+        {
+            List<string> keys = new List<string>(this.commandsMap.Keys);
+            var commands = String.Join(", ", keys.ToArray());
+            Console.WriteLine(commands);
+        }
+
+    }
+}
--- StoreCore/Program.cs
+++ StoreCore/Program.cs
@@ -8,57 +8,19 @@ namespace StoreCore
 {
     class Program
     {
-
-        protected static Dictionary<string, IConsoleUI> consoleUIs;
-        protected static Dictionary<string, Action> commandsMap;
-
         static void Main(string[] args)
         {
-            config();
-            Console.WriteLine("Welcome to our store!\r\nHow can we help you?");
-            Console.WriteLine("To display a list of all available commands type: help");
+            Config();
 
-            while (true)
-            {
-                String input = Console.ReadLine();
-
-                if (input == "exit")
-                    break;
-                else if (commandsMap.ContainsKey(input))
-                {
-                    commandsMap[input]();
-                }
-                else
-                    Console.WriteLine("Incorrect command. Type 'help' for a list of commands. Type 'exit' to exit the program.");
-            }
-
+            MainUI mainUI = new MainUI();
+            mainUI.Start();
         }
 
-        static void config()
+        static void Config()
         {
             System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
             customCulture.NumberFormat.NumberDecimalSeparator = ".";
             System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
-
-            Program.consoleUIs = new Dictionary<string, IConsoleUI>();
-            Program.consoleUIs.Add("UserUI", new UserUI());
-            Program.consoleUIs.Add("ProductUI", new ProductUI());
-
-            Program.commandsMap = new Dictionary<string, Action>();
-            foreach (var consoleUI in consoleUIs)
-            {
-                consoleUI.Value.registerCommands(Program.commandsMap);
-            }
-
-            commandsMap.Add("help", listCommands);
         }
-
-        static void listCommands()
-        {
-            List<string> keys = new List<string>(commandsMap.Keys);
-            var commands = String.Join(", ", keys.ToArray());
-            Console.WriteLine(commands);
-        }
-
     }
 }
