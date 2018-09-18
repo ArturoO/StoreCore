46f84cbea41fbb5f332c35d93271467ba9779d02
Artur Hoffman <thearturoo87@gmail.com>
2018-09-18 21:47:59 +0200

    Add EntityFramework to project. Create context class and User2 class.

---
+++ StoreCore/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreCore
+{
+    class StoreContext : DbContext
+    {
+        public DbSet<User2> Users { get; set; }        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=ARTUROO-PC;Initial Catalog=Store2;Integrated Security=True;Pooling=False");
+        }
+    }
+}
